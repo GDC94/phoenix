ef810757fc6c55cb0932e2f864f98319e2770159
doctorbilardo <g.derbes@paisanos.io>
2024-05-03 01:11:25 -0300

    feat: adding coding style tools

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,22 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { resolve } from 'path'
-import dts from 'vite-plugin-dts'
-import { configDefaults } from 'vitest/config'
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react-swc";
+import {resolve} from "path";
+import dts from "vite-plugin-dts";
+import {configDefaults} from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ outDir: 'dist', include: ["src/index.ts"], exclude: ["src/**/__tests__/**"] })],
+  plugins: [react(), dts({outDir: "dist", include: ["src/index.ts"], exclude: ["src/**/__tests__/**"]})],
 
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: "jsdom",
     css: true,
-    setupFiles: './tests/setup.ts',
+    setupFiles: "./tests/setup.ts",
     coverage: {
-      include: ['src/ui/**/*.{ts,tsx}'],
-      exclude: [
-        ...configDefaults.exclude,
-        'src/ui/**/**/*.stories.{ts,tsx}'
-      ],
+      include: ["src/ui/**/*.{ts,tsx}"],
+      exclude: [...configDefaults.exclude, "src/ui/**/**/*.stories.{ts,tsx}"],
       thresholds: {
         lines: 30,
         functions: 30,
@@ -30,18 +27,18 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'lvlup-components',
-      formats: ['es', 'umd']
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "lvlup-components",
+      formats: ["es", "umd"]
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ["react", "react-dom"],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
+          react: "React",
+          "react-dom": "ReactDOM"
         }
       }
     }
   }
-})
+});
